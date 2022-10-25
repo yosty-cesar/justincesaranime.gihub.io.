7946fcda918c76b79dd33f35fe116a3c115d30a7
yosty cesar llanos <116007553+yosty-cesar@users.noreply.github.com>
2022-10-25 17:38:41 -0400

    Initial commit

---
+++ prueba java/calculadora.java
@@ -0,0 +1,24 @@
+
+public class calculadora {
+ 
+    public static void main(String[] args) {
+ 
+        //Declaramos las variables
+        int num1=10;
+        int num2=30;
+ 
+        /*Realizamos las operaciones.
+         * Tambien lo podemos guardar el resultado en variables. */
+ 
+        System.out.println("El resultado de la suma es "+(num1+num2));
+ 
+        System.out.println("El resultado de la resta es "+(num1-num2));
+ 
+        System.out.println("El resultado de la multiplicación es "+(num1*num2));
+ 
+        System.out.println("El resultado de la división es "+(num1/num2));
+    }
+}
+
+
+ 
